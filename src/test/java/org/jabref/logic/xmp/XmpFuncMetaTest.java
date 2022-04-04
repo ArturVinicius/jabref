7b2ea43a2413e5db973ed4fbb2155df442dce826
Artur Vinicius <arturv98@gmail.com>
2022-04-04 19:10:30 -0300

    Updating test class XmpFuncMeta

--- src/test/java/org/jabref/logic/xmp/XmpFuncMetaTest.java
+++ src/test/java/org/jabref/logic/xmp/XmpFuncMetaTest.java
@@ -2,13 +2,17 @@ package org.jabref.logic.xmp;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class XmpFuncMetaTest {
 
+    Random random= new Random();
+
     @Test
     public void VariavelNull(){
-        int opcao = 0;
+        int opcao = random.nextInt(4);
         XmpFuncMeta teste = new XmpFuncMeta("title","author","Subject","data");
         String result = teste.retiraVariavel(opcao);
         assertEquals(null, result);
